9ad37a26ccea54514263578890f526b1963f2fc4
Kaiser <kaiserfromwork@gmail.com>
2025-08-16 19:55:51 +0100

    implemented code to get a random quote from API

--- cli_pomodoro.py
+++ cli_pomodoro.py
@@ -20,17 +20,47 @@ def count_down_timer(interval_time: int):
         time.sleep(1)
 
 
+def check_digits(focused_interval: str, break_interval: str) -> bool:
+    errors_list = []
+    if not focused_interval.isdigit():
+        errors_list.append("Please, enter only numbers for your focused time.\n")
+    if not break_interval.isdigit():
+        errors_list.append("Please, enter only numbers for  your break time.")
+
+    is_valid = not errors_list
+    return is_valid
+
+
 def cli_pomodoro():
+    focused_interval = input(
+        "How long would you like to work for each time? (in minutes)"
+    )
+    break_interval = input(
+        "How long would you like to break between sessions? After 2 focus session you will have a longer break."
+    )
+
+    if not focused_interval.isdigit():
+        print("Please, enter only numbers for your focused time.\n")
+    if not break_interval.isdigit():
+        print("Please, enter only numbers for  your break time.")
+
     seconds = 60
-    focused_time = 5 * seconds
-    short_break = 2 * seconds
+    focused_time = int(focused_interval) * seconds
+    short_break = int(break_interval) * seconds
+    counter = 0
 
     while True:
+        if counter == 2:
+            short_break = +short_break
+            counter = 0
+        else:
+            short_break = short_break
         count_down_timer(focused_time)
         print("\nYour Focused time is UP!.\nYou can take a short break now!\n")
         notify_user()
         print("BREAK TIME!")
         count_down_timer(short_break)
+        counter = +1
 
 
 if __name__ == "__main__":
---
+++ quotes.py
@@ -0,0 +1,21 @@
+import json
+import requests
+
+
+def get_quote() -> tuple | None:
+    try:
+        response = requests.get("https://zenquotes.io/api/random")
+        data = response.json()
+        quote = data[0]["q"]
+        author = data[0]["a"]
+
+        return quote, author
+
+    # to catch http/network errors
+    except requests.exceptions.RequestException as error:
+        print(f"Error while fetching data from url: {error}")
+
+    # to catch error if response is json
+    except json.JSONDecodeError as error:
+        print(f"Error while decoding file: {error}")
+        return None
